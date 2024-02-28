86fe5039f7879f68627e1a70aa9a5007a7196e79
Vladimir Yablokov <YablokovWork@gmail.com>
2024-02-28 21:14:01 +0300

    changes speedfall

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-let count = 10;
+let count = 50;
 let elems = ["flake.gif"];
 let y = []
 let x = []
@@ -10,19 +10,19 @@ let width = document.body.scrollWidth;
 
 for (i = 0; i < count; i++) {
     let pic = "./img/" + elems[Math.floor(Math.random() * elems.length)];
-    let snow = document.createElement('img');
-    snow.id = "snow" + i;
-    snow.src = pic;
-    snow.style.position = "absolute";
-    document.body.appendChild(snow);
+    let elem = document.createElement('img');
+    elem.id = "elem" + i;
+    elem.src = pic;
+    elem.style.position = "absolute";
+    document.body.appendChild(elem);
 }
 
 for (i = 0; i < count; i++) {
     y[i] = Math.round(Math.random() * height);
     x[i] = Math.round(Math.random() * width) - 90;
-    speed[i] = Math.random() * 5 + 2;
     xstep[i] = 0;
-    step[i] = Math.random() * 0.1 + 0.05;
+    speed[i] = Math.random() * 1.5 + 1;
+    step[i] = Math.random() * 0.01 + 0.05;
 }
 
 function fall() {
@@ -30,23 +30,21 @@ function fall() {
     let width = document.body.scrollWidth;
     for (i = 0; i < count; i++) {
         let sx = speed[i] * Math.atan(xstep[i]);
-
         if (x[i] + sx > width - 50) {
             x[i] = sx
         };
-
         x[i] += sx;
-        y[i] += speed[i] * Math.sin(Math.PI / 2);
+        y[i] += speed[i] * Math.atan(Math.PI / 2);
 
-        if (y[i] > height - 60) {
+        if (y[i] > height - 50) {
             y[i] = 0;
-            x[i] = Math.random() * width - 89;
+            x[i] = Math.random() * width - 50;
             step[i] = Math.random() * 0.1 + 0.05;
-            speed[i] = Math.random() * 5 + 2;
+            speed[i] = Math.random() * 1 + 1;
         }
 
-        document.getElementById("snow" + i).style.left = x[i] + "px";
-        document.getElementById("snow" + i).style.top = y[i] + "px";
+        document.getElementById("elem" + i).style.left = x[i] + "px";
+        document.getElementById("elem" + i).style.top = y[i] + "px";
         xstep[i] += step[i];
     }
     setTimeout("fall()", 30);
